685455032c383a73b0893503680bc5567c1a2edf
Lutfur Rahman Sarker <eng.lrsarker@gmail.com>
2020-03-03 01:25:35 +0600

    ES6 Template javascript

---
+++ template.js
@@ -0,0 +1,15 @@
+const firstName = "Lutfur Rahman";
+const lastName = "Sarker";
+const fullName = firstName + " " + lastName + " is a good boy";
+const fullNameES6 = `${firstName} ${lastName} is a good boy`;
+console.log(fullNameES6);
+//Multi Line
+const multiLine = "I Love Programming\n"
++ "I Love Codding\n"
++"I Love gaming";
+console.log(multiLine);
+//ES6 Multi Line
+const multiLineEs6 = `I Love Programming
+I Love Gaming
+I Love Codding.`;
+console.log(multiLineEs6);
